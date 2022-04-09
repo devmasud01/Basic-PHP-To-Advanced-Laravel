613956f5ea8aa9a242a182e67ae9bcc3f870e418
Masud Hasan <devmasud01@gmail.com>
2022-04-09 12:23:23 +0600

    1

--- 499-Assinment-Class-05/index.php
+++ 499-Assinment-Class-05/index.php
@@ -2,20 +2,11 @@
 $array = [12, 34, 2, 6, 78];
 
 foreach ($array as $key => $mixval) {
-    echo $mixval;
-    "<br>";
-    for($x=2; $x<$n; $x++)
-   {
-      if($n %$x ==0)
-	      {
-		   return 0;
-		  }
-
-          if (condition) {
-              # code...
-          }
-}
-
-for ($i=0; $i < ; $i++) { 
-    # code...
-}
+    if ($mixval%2==0  && $mixval>2) {
+        continue;
+    }
+    else {
+        echo "$mixval -- is a Prime Number";
+        "<br>";
+    }  
+} 

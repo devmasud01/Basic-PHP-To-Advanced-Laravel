82422fad96a894cdc2969b5b05dec9c6188b21e0
Masud Hasan <devmasud01@gmail.com>
2022-04-10 10:15:38 +0600

    8

--- 499-Assinment-Class-05/index.php
+++ 499-Assinment-Class-05/index.php
@@ -1,6 +1,7 @@
 <?php
 
-echo "<h3>Ans To The Ques. No- One</h3>";
+echo "<h2>Ans To The Ques. No- One</h2>";
+echo "<h3>We know the array which is represent by an index number by default, which starts from 0 is called index array. This is an indexed array. PHP indexed array is also known as numeric array.</h3>";
 
 $array = [12, 34, 2, 6, 78];
 
@@ -13,3 +14,5 @@ foreach ($array as $key => $mixval) {
         "<br>";
     }  
 } 
+
+echo "<h2>Ans To The Ques. No- Two</h2>";

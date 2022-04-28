ee1c22da08583cdbd27a89516db84c5c95ee8959
Masud Hasan <devmasud01@gmail.com>
2022-04-28 20:54:30 +0600

    4-28-2022

--- 499-Assinment-Class-08/index.php
+++ 499-Assinment-Class-08/index.php
@@ -1,59 +1,73 @@
 <?php
 
-$age =20;
-function ageclass($age){
-    if ($age>=3 && $age<=13) {
-        echo "Amne miya Children";
-    }
-    elseif ($age>13 && $age<=19) {
-        echo "Amne miya Teenage";
-    }
-    elseif ($age>19 && $age<=24) {
-        echo "Amne miya Young";
-    }
-    elseif ($age>=60) {
-        echo "Amne miya Young";
-    }
-};
-ageclass($age);
-
-// $length = 10;
-// $widht = 12;
-// $height = 6;
-// function area($length, $widht, $height){
-//     if (){
-//         echo "Area of Rectangle is : " . $length*$widht;
+// $age =20;
+// function ageclass($age){
+//     if ($age>=3 && $age<=13) {
+//         echo "Amne miya Children";
 //     }
-    
+//     elseif ($age>13 && $age<=19) {
+//         echo "Amne miya Teenage";
+//     }
+//     elseif ($age>19 && $age<=24) {
+//         echo "Amne miya Young";
+//     }
+//     elseif ($age>=60) {
+//         echo "Amne miya Young";
+//     }
+// };
+// ageclass($age);
+
+// function area_rectangle($length, $widht){
+//     $areaOfRectangle = ($length * $widht);
+//     return( $areaOfRectangle);
+// };
+// echo area_rectangle(10, 10);
+
+// function area_square($length){
+//     $areaOfSquare =($length*$length);
+//     return($areaOfSquare);
+// }
+// echo area_square(10);
+
+// function area_triangle($base, $height){
+//     $areaOfTriangle = (1/2)*($base*$height);
+//     return($areaOfTriangle);
+// }
+// echo area_triangle(10, 40);
+
+
+// $parcentige = 10;
+// function gpa_grader($parcentige){
+//     if ($parcentige>=80) {
+//         echo "You got GPA-A+";
+//     }
+//     elseif ($parcentige>=70) {
+//         echo "You got GPA-A";
+//     }
+//     elseif ($parcentige>=60) {
+//         echo "You got GPA-A-";
+//     }
+//     elseif ($parcentige>=50) {
+//         echo "You got GPA-B";
+//     }
+//     elseif ($parcentige>=40) {
+//         echo "You got GPA-C";
+//     }
+//     elseif ($parcentige<=33) {
+//         echo "You Failed";
+//     }
+//     else{
+//         echo "Enter a valid number";
+//     }
 
 // };
-// area($length, $widht, $height);
+// gpa_grader($parcentige);
 
-$parcentige = 10;
-function gpa_grader($parcentige){
-    if ($parcentige>=80) {
-        echo "You got GPA-A+";
+ function ageCalc($dateOfBirth)
+    {
+        $dob = ($dateOfBirth);
+        $now = date("Y-m-d");
+        $age = date_diff(date_create($dob), date_create($now));
+        return($age->format("%y"));
     }
-    elseif ($parcentige>=70) {
-        echo "You got GPA-A";
-    }
-    elseif ($parcentige>=60) {
-        echo "You got GPA-A-";
-    }
-    elseif ($parcentige>=50) {
-        echo "You got GPA-B";
-    }
-    elseif ($parcentige>=40) {
-        echo "You got GPA-C";
-    }
-    elseif ($parcentige<=33) {
-        echo "You Failed";
-    }
-    else{
-        echo "Enter a valid number";
-    }
-
-};
-gpa_grader($parcentige);
-
-
+   echo ageCalc("17-10-1998");
